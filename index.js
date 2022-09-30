ba69873289d3bc46d773e80284bfacab333d070e
Davi Marcilio <davimarcilio.js@gmail.com>
2022-09-30 18:56:35 -0300

    testes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter');
 const adminRouter = require('./routes/adminRouter');
+const pageRouter = require('./routes/pageRouter');
 const mongoose = require('mongoose');
+app.set('view engine', 'ejs');
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
     if (error) {
         console.log(error);
@@ -12,13 +14,14 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useU
     }
 
 });
-app.use('*', (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        next();
-    } else {
-        res.redirect('https://' + req.headers.host + req.originalUrl);
-    }
-})
+// app.use('*', (req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] === 'https') {
+//         next();
+//     } else {
+//         res.redirect('https://' + req.headers.host + req.originalUrl);
+//     }
+// })
+app.use('/', express.json(), pageRouter);
 app.use('/user', express.json(), userRouter);
 app.use('/admin', express.json(), adminRouter);
 app.listen(process.env.PORT, () => {
